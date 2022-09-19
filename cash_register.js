42ff30ed9d463c61fc02714614b76eb097ad5581
Anthony Leadbetter <aleadbetter93@gmail.com>
2022-09-19 06:02:47 +0000

    Added checkInputVars to Cash_Register

--- cash_register.js
+++ cash_register.js
@@ -1,6 +1,16 @@
 function checkCashRegister(price, cash, cid) {
-    let change;
-    return change;
-  }
-  
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+    //Check vars work...
+    checkInputVars(price, cash, cid);
+    // totalCashInDraw(cid);
+}
+
+
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+function checkInputVars (inputPrice, inputCash, inputCid) {
+    console.log("Here are my input vars \nThe price: " + inputPrice);
+    console.log("The cash: " + inputCash);
+    console.log("The cid: ");
+    console.table(inputCid);
+}
--- node_testing.js
+++ node_testing.js
@@ -1,6 +1,23 @@
 function checkCashRegister(price, cash, cid) {
-    console.log("The price: " + price + "\n")
-    console.log("The cash: " + cash + "\n")
-    console.log("The cid: " + cid + "\n")
+    //Check vars work...
+    checkInputVars(price, cash, cid);
+    totalCashInDraw(cid);
 }
 checkCashRegister(2, 1, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+
+function totalCashInDraw (cashTable){
+    let totalCash = 0;
+    for (let i = 0; i <= cashTable.length; i++){
+        // totalCash += cashTable[i][1];
+        console.log(cashTable[i][1]);
+    }
+    console.log("Total Cash in the draw = " + totalCash);
+}
+
+function checkInputVars (inputPrice, inputCash, inputCid) {
+    console.log("Here are my input vars \nThe price: " + inputPrice);
+    console.log("The cash: " + inputCash);
+    console.log("The cid: ");
+    console.table(inputCid);
+}
