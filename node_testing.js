b657240fc1aa39ed316fb851860c6a412d34ebf2
Anthony Leadbetter <aleadbetter93@gmail.com>
2022-09-20 15:38:35 +0000

    not enough change coniditional edited with object

--- node_testing.js
+++ node_testing.js
@@ -6,9 +6,10 @@ function checkCashRegister(price, cash, cid) {
     checkCashInDraw(price, cash, cid);
 }
 
-checkCashRegister(19.5, 19.5, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 600000, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
 function checkCashInDraw (productPrice, customerCash, registerTally){
+    // Determine amount in draw
     let totalCash = 0;
     for (let i = 0; i < registerTally.length; i++){
         totalCash += registerTally[i][1];
@@ -16,15 +17,25 @@ function checkCashInDraw (productPrice, customerCash, registerTally){
     let totalCashRounded = totalCash.toFixed(2);
     console.log("Total Cash in the draw = " + totalCashRounded);
     
+    // build the object
+    const fccObject = {
+        status: "",
+        change: ""
+    };
+    // Evaluate difference between change needed and money in till
     let changeNeeded = customerCash - productPrice;
     let cashDifference = totalCashRounded - changeNeeded;
 
-    // determine if we need to calculate change, respond accordingly
+    // determine if we need to calculate change, respond accordingly... we may need to revisit this if we don't have the right COINS for change too
     if (cashDifference < 0){
         console.log(`We literally don't have enough money to cover your change: $${changeNeeded}`);
+        fccObject["status"] = "INSUFFICIENT_FUNDS";
+        fccObject["change"] = [];
+        console.log(`The object value is: ` + JSON.stringify(fccObject));
+        return fccObject;
         // return object line; 
     }
-    else if(cashDifference = 0){
+    else if(cashDifference == 0){
         console.log(`We have the EXACT amount you need: $${changeNeeded}`);
         // return object line;
     }
---
+++ scratch_pad.js
@@ -0,0 +1,10 @@
+// You can't just print objects, need to use stringify..... lol
+/*
+const myObject = {
+    key1: "some letters",
+    key2: 3123
+};
+
+console.log(`my object's output is ${myObject}`);
+console.log(JSON.stringify(myObject));
+*/
